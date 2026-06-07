3e1107e19eac82b2547f09a5720d28f0e37e92b8
Alexandro <61099291@mail.isil.pe>
2026-06-07 06:46:00 -0500

    PA3 Terminada

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory; //esto por lo que entendi es para decirle donde esta la herramienta y asi no se loquee buscandola
 use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    use HasFactory;
+    use HasFactory; //esto lo vi en su ejemplo y investigando me sirve para generar datos falsos de forma automatica para llenar con datos mi base de datos
+                    // lo agregue por si acasito
 
     protected $table = 'categorias';
 
